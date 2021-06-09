60872278e06d6a9b30cf00c9354e124ac9b787f2
eldar <eldartakach@gmail.com>
2021-06-09 13:07:19 +0200

    Comments

--- Telecontrol.py
+++ Telecontrol.py
@@ -1,53 +1,111 @@
+
+########################################################################
+########################################################################
+############### Nexstar 8SE Telescope Ground Control API ###############
+########################################################################
+########################################################################
+
+
+
+"""
+This API is used to controll the Nexstar 8SE Telescope from the ground using Python
+"""
+
 import serial
-#from threading import *
 import threading as tr
 import time
 import math
 
 two_inTwentyFour=16777216
 
-
-
-
 class Telcontrol(tr.Thread):
-#everythin in degrees
+    """ The main class, all function that enables the telescope control can be found here 
+    
+    ...
 
-    telescopeX_Direction = 0; #right,stop,left
-    telescopeX_Speed = 0;
+    Attributes
+    ----------
+    telescopeX_Direction : int
+        The target of the telescope on the X axis, in pixels.
+    telescopeX_Speed : int  
+        Speed of the telescope's movement on the X axis, [0,9] - the higher the faster
+    telescopeY_Direction : int
+        The target of the telescope on the Y axis, in pixels.
+    telescopeY_Speed : int
+        Speed of the telescope's movement on the Y axis, [0,9] - the higer the faster
+    
+    Methods
+    -------
+    Telecontol (init) (self,port,baudrate,mindx,mindy,maxdx,maxdy)
+        Inits the Telecontrol
+    connect(self)
+        Start the connection to the telescope
+    disconnect(self)
+        End the connection to the telescope
+    stop(self)
+        Stops the telescope's movement on both axiss
+    moveX(self,direction,speed)
+        Move the telescope on the X axis towards pixel direction(int) in the speed specified(int[0-9])
+    moveY(self,direction,speed)
+        Move the telescope on the X axis towards pixel direction(int) in the speed specified(int[0-9])
+    goToZero(self)
+        Move the telescope to [0,0]
+    manualLeft(self,speed)  
+        Go left in speed specified(int[0-9])
+    manualRight(self,speed)  
+        Go right in speed specified(int[0-9])
+    manualUp(self,speed)  
+        Go up in speed specified(int[0-9])
+    manualDown(self,speed)  
+        Go down in speed specified(int[0-9])
+    
+    """
+    telescopeX_Direction = 0
+    telescopeX_Speed = 0
+    telescopeY_Direction = 0 
+    telescopeY_Speed = 0
 
-    telescopeY_Direction = 0;
-    telescopeY_Speed = 0;
+    def __init__(self,port = 'com8' ,baudrate="9600", mindx=10, mindy=10, maxdx=1000, maxdy=1000): 
+        """ Init the Telescope
 
-    #def __init__(self,port="/dev/ttyUSB0", baudrate="9600", mindx=10, mindy=10,maxdx=1000, maxdy=1000):
-    def __init__(self,port = 'com8' ,baudrate="9600", mindx=10, mindy=10, maxdx=1000, maxdy=1000):
-        tr.Thread.__init__(self);
-        #self.ser = serial.Serial(port, baudrate)  # need to check the com
+        Parameters
+        ----------
+        port : string
+            for Windows port should be 'com[x]', x is a number, look for it in the Device Manager, without []'s
+            for Linux port should be '/dev/ttyUSB[x]',x is a number, look for it in the 'dev' folder, without []'s
+        baudrate : string
+            number of bits being sent at a time
+        mindx : int
+            min pixel in x axis
+        mindy : int
+            min pixel in y axis
+        maxdx : int
+            max pixel in x axis
+        maxdy : int
+            max pixel in y axis
 
-        self.ser = serial.Serial()
-        self.ser.port = 'com8'  # may be called something different
-        self.ser.baudrate = 9600  # may be different
-        isopenend = self.ser.open()
+        """
+    tr.Thread.__init__(self);
+    self.ser = serial.Serial()
+    self.ser.port = 'com8'  # may be called something different
+    self.ser.baudrate = 9600  # may be different
+    isopenend = self.ser.open()
 
-        print("Connected to Telescope Controll");
+    print("Connected to Telescope Controll");  
+    self.initProtocol()
+    self.mindx=mindx
+    self.mindy=mindy
+    self.maxdy=maxdy
+    self.maxdx=maxdx
+    self.isReady=True
+    self.whileLock = tr.Semaphore(value=0)
+    self.readyLock = tr.Lock()
+    self.diffaz = 0
+    self.diffelv = 0
+    self.start()
 
-        # x = ser.is_open
-        self.initProtocol()
-        self.mindx=mindx
-        self.mindy=mindy
-        self.maxdy=maxdy
-        self.maxdx=maxdx
-        self.isReady=True
-        self.whileLock = tr.Semaphore(value=0)
-        self.readyLock = tr.Lock()
-        #self.lock.acquire()
-        self.diffaz = 0
-        self.diffelv = 0
-        self.start()
-
-        # self.resetAzmTelescope()
-        # self.resetAltTelescope()
-        self.oldx=0
-        self.oldy=0
+    self.oldx=0
+    self.oldy=0
 
 
     def angle_to_24bit(self,ang):
@@ -89,6 +147,7 @@ class Telcontrol(tr.Thread):
 
 
     def connect(self):
+        """ Connects the telescope """
         if not self.ser.is_open:
             self.ser.open()
 
@@ -102,7 +161,6 @@ class Telcontrol(tr.Thread):
         self.stop_r = chr(80) + chr(2) + chr(16) + chr(36) + chr(0) + chr(0) + chr(0) + chr(0)  # Stop
         self.stop_l = chr(80) + chr(2) + chr(16) + chr(37) + chr(0) + chr(0) + chr(0) + chr(0)
 
-        #command3 = chr(80) + chr(2) + chr(16) + chr(37) + chr(7) + chr(142) + chr(56) + chr(0)  # left (reset)
         self.midUp = chr(80) + chr(2) + chr(17) + chr(36) + chr(6) + chr(142) + chr(56) + chr(0)  # up
         self.midDown = chr(80) + chr(2) + chr(17) + chr(37) + chr(6) + chr(142) + chr(56) + chr(0)  # down
         self.Stop_d = chr(80) + chr(2) + chr(17) + chr(37) + chr(0) + chr(0) + chr(0) + chr(0)  # stop
@@ -193,36 +251,15 @@ class Telcontrol(tr.Thread):
             self.ser.write(self.Stop_u.encode())
             print("stop y")
     def stop_x(self):
+        """ Stops the telescope's movement on the x axis """
         self.ser.write(self.stop_r.encode())
         self.ser.write(self.stop_l.encode())
 
     def stop_y(self):
+        """ Stops the telescope's movement on the y axis """
         self.ser.write(self.Stop_d.encode())
         self.ser.write(self.Stop_u.encode())
 
-            #time.sleep(0.1)
-        #time.sleep(math.abs(self.dy / self.maxdy))
-        #print("up down")
-
-
-
-    # def correct(self):
-    #     print("correct")
-    #     print(math.fabs(self.diffaz), "fabs")
-    #     if ( math.fabs(self.diffaz)> self.maxdx):
-    #         self.setAzimut(self.myAz)
-    #         self.diffaz=0
-    #         print("myaz- move",self.myAz)
-    #         #time.sleep(0.1)
-    #     if (math.fabs(self.diffelv)> self.maxdy):
-    #         self.setAltitude(self.myElv)
-    #         self.diffelv=0
-    #         print("myelv-move",self.myElv)
-    #         #time.sleep(0.1)
-    #     #time.sleep(math.abs(self.dy / self.maxdy))
-    #     #print("up down")
-
-    # move up
     def run(self):
         while True:
             self.whileLock.acquire()
@@ -238,6 +275,7 @@ class Telcontrol(tr.Thread):
     #    b12AB0500,40000500
 
     def goToZero(self):
+        """ Move the telescope the [0,0] """
         moveToZero = "b00000000,00000000"
         self.ser.write(moveToZero.encode())
         time.sleep(0.1)
@@ -266,29 +304,55 @@ class Telcontrol(tr.Thread):
 
 
     def manualRight(self,speed=3):
+        """ Go right in the speed specified 
+    Parameters
+    ----------
+    speed : int
+        the speed of the movement of the telescope to the right
+        """
         moveRight = chr(80) + chr(2) + chr(16) + chr(36) + chr(speed) + chr(0) + chr(0) + chr(0)
         self.ser.write(moveRight.encode())
 
     def manualLeft(self,speed=3):
+        """ Go left in the speed specified 
+    Parameters
+    ----------
+    speed : int
+        the speed of the movement of the telescope to the left
+        """
         moveLeft = chr(80) + chr(2) + chr(16) + chr(37) + chr(speed) + chr(0) + chr(0) + chr(0)
         self.ser.write(moveLeft.encode())
 
     def manualUp(self,speed=3):
+        """ Go up in the speed specified 
+    Parameters
+    ----------
+    speed : int
+        the speed of the movement of the telescope upwards
+        """
         moveUp = chr(80) + chr(2) + chr(17) + chr(36) + chr(speed) + chr(0) + chr(0) + chr(0)
         self.ser.write(moveUp.encode())
 
     def manualDown(self,speed=3):
+        """ Go down in the speed specified 
+    Parameters
+    ----------
+    speed : int
+        the speed of the movement of the telescope downwards
+        """
         moveDown = chr(80) + chr(2) + chr(17) + chr(37) + chr(speed) + chr(0) + chr(0) + chr(0)
         self.ser.write(moveDown.encode())
 
 
     def stopX(self):
+        """ Stops the movement of the telescope on the X axis """
         print("stop x")
         stopMoveX = chr(80) + chr(2) + chr(16) + chr(36) + chr(0) + chr(0) + chr(0) + chr(0)
         self.ser.write(stopMoveX.encode())
         time.sleep(0.1)
 
     def stopY(self):
+        """ Stops the movement of the telescope on the Y axis """
         print("stop y")
         stopMoveY = chr(80) + chr(2) + chr(17) + chr(36) + chr(0) + chr(0) + chr(0) + chr(0)
         self.ser.write(stopMoveY.encode())
@@ -301,6 +365,14 @@ class Telcontrol(tr.Thread):
         self.stopY()
 
     def moveX(self,direction,speed):
+        """ Moves the telescope on the X axis
+        Parameters
+        ----------
+        direction : int
+            the target of the telescope on the x axis in pixels
+        speed : int 
+            the speed of the telescope's movement on the x axis in int
+            """
 
         if abs(direction)<2:
             if (self.telescopeX_Speed != 0):
@@ -318,6 +390,14 @@ class Telcontrol(tr.Thread):
                     self.manualLeft(speed)
 
     def moveY(self,direction,speed):
+        """ Moves the telescope on the Y axis
+        Parameters
+        ----------
+        direction : int
+            the target of the telescope on the Y axis in pixels
+        speed : int 
+            the speed of the telescope's movement on the Y axis in int
+            """
 
         if abs(direction)<2:
             if (self.telescopeY_Speed != 0):
@@ -333,22 +413,10 @@ class Telcontrol(tr.Thread):
                     self.manualDown(speed)
                 else:
                     self.manualUp(speed)
-    # def manualRight(self):
-    #     self.ser.write(self.goRight_9)
-    #     time.sleep(0.2)
-    #     self.ser.write(self.StopX)
-    # def manualLeft(self):
-    #     self.ser.write(self.goLeft_9)
-    #     time.sleep(0.2)
-    #     self.ser.write(self.StopX)
-    # def manualUp(self):
-    #     self.ser.write(self.goUp)
-    #     time.sleep(0.2)
-    #     self.ser.write(self.StopX)
-    # def manualDown(self):
-    #     self.ser.write(self.goDown)
-    #     time.sleep(0.2)
-    #     self.ser.write(self.StopX)
-    def imidiateStop(self):
-        self.ser.write(chr(76).encode())
 
+########################################################################
+########################################################################
+###################### Written by Semyon Pikollov ######################
+######## Documented by Eldar Takach, Dor Getter and Oren Lacker ########
+########################################################################
+########################################################################
